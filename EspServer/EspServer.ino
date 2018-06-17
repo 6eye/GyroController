b077ca2d2353b88533c1f964b1b92ac95279b3d0
miosz <milblasiak@gmail.com>
2018-06-17 02:37:52 +0200

    Esp Server |Single client

---
+++ EspServer/EspServer.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>
+//Single client server
+
+//Store all setting in varaiables for easier modification
+const char* ssid     = "NAME";      // SSID | WiFi network name
+const char* password = "PASSWORD";      // Network Password
+
+const int   port = 8080;           
+WiFiServer server(8080);  //Set server port for 8080 | 1024-49151 
+
+void setup() {
+  //Start comunication at serial lines with 115200 bits/sec
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password); //Sart and connect to wifi
+  //Wait untill conection is establisehd
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    
+  }
+
+ 
+  //Start server
+  server.begin();
+ 
+
+}
+
+void loop() {
+  //Wait for client to connect
+  WiFiClient client = server.available();
+  if(client){
+    
+    while(client.connected()){
+      //While client is conected and || Serial buffer contains data send read data  to client
+      if(Serial.available()){
+     
+      client.println(Serial.read());
+      }
+    }
+  }
+  
+}
