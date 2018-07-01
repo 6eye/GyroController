2ac4597aa3375fa482cb57ffdc9eaaa074fa7e94
miosz <milblasiak@gmail.com>
2018-07-01 19:25:10 +0200

    Buffering on Esp | Tested | Working

--- EspServer/EspServer.ino
+++ EspServer/EspServer.ino
@@ -2,8 +2,10 @@
 //Single client server
 
 //Store all setting in varaiables for easier modification
-const char* ssid     = "NAME";      // SSID | WiFi network name
-const char* password = "PASSWORD";      // Network Password
+const char* ssid     = "--------";      // SSID | WiFi network name
+const char* password = "--------";      // Network Password
+
+
 
 const int   port = 8080;           
 WiFiServer server(8080);  //Set server port for 8080 | 1024-49151 
@@ -11,6 +13,7 @@ WiFiServer server(8080);  //Set server port for 8080 | 1024-49151
 void setup() {
   //Start comunication at serial lines with 115200 bits/sec
   Serial.begin(115200);
+
   
   WiFi.begin(ssid, password); //Sart and connect to wifi
   //Wait untill conection is establisehd
@@ -30,12 +33,30 @@ void loop() {
   //Wait for client to connect
   WiFiClient client = server.available();
   if(client){
-    
+       String data_buffer="";
+       char   char_buffer;
+
+    //Whirl clieng is conected do ...
     while(client.connected()){
-      //While client is conected and || Serial buffer contains data send read data  to client
+
+
+      
       if(Serial.available()){
-     
-      client.println(Serial.read());
+          char_buffer=char(Serial.read()); //Read Single Char form Serial
+          
+          if(char_buffer=='(' ){  //If char is '(' then new data package is coming || Reset data_buffer
+            data_buffer="";
+          }
+          else{
+            if(char_buffer==')'){  //If cahr is ')' it is end of data package || So send data to client
+              //Maybe add some error detection here in futere
+              client.println(data_buffer);
+              
+            }else{
+              data_buffer=data_buffer+char_buffer;
+            } 
+          }
+         
       }
     }
   }
